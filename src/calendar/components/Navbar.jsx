7dc936678ed87bcafc3f1f7342a2b029cc90d1b0
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2025-05-13 12:20:27 -0500

    ADD: React Big Calendar

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,13 +1,13 @@
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark mb-4">
+    <nav className="navbar navbar-dark bg-dark mb-4 px-4">
       <span className="navbar-brand">
         <i className="fas fa-calendar-alt"></i>
         &nbsp; Jorge Arias
       </span>
       <button className="btn btn-outline-danger">
         <i className="fas fa-sign-out-alt"></i>
-        <span>Logout</span>
+        <span>Log Out</span>
       </button>
     </nav>
   );
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,49 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
 import { Navbar } from "../components/Navbar";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const myPropsEvent = [
+  {
+    title: "event 1",
+    notes: "notes",
+    start: new Date(),
+    end: addHours(new Date(), 1),
+    user: {
+      id: 1,
+      name: "Jorge Arias",
+    },
+    bgColor: "#fafafa",
+    borderColor: "#009dff",
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={myPropsEvent}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)", width: "100%" }}
+      />
     </>
   );
 };
