83a39464fb3d7f2495e0d1d71982ca072c618d77
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2025-05-20 15:47:02 -0500

    ADD: Install and Config Redux, Redux-Toolkit

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router";
 import { AppRouter } from "./router";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const CalendarApp = () => {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
